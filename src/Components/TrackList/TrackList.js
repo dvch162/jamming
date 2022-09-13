1d0e306ee5ba9429b3937c17736bb7ff3e8ebf9b
Dmytro Che <deefordis8@gmail.com>
2022-09-13 21:29:30 +0300

    Sect 2

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './TrackList.css';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div class="TrackList">
+            </div>
+        )
+    }
+}
+
+export default TrackList;
