89754086fa48e4d5a38fc99d141d897560e6ef3b
Dmytro Che <deefordis8@gmail.com>
2022-09-14 18:38:44 +0300

    Sec 5 Add track

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -48,6 +48,19 @@ class App extends React.Component {
       id: 6}]
 
     }
+    this.addTrack = this.addTrack.bind(this);
+
+
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -57,7 +70,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
+            <SearchResults searchResults={this.state.searchResults}
+                            onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks} />
           </div>
