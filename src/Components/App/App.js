1395535b5c4738379e642c613d9526ef89bcb953
Dmytro Che <deefordis8@gmail.com>
2022-09-14 15:53:06 +0300

    Section 4

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -29,7 +29,24 @@ class App extends React.Component {
         album: 'album3', 
         id: 3 
       }
-    ]
+    ],
+      playlistName: 'My Playlist',
+      playlistTracks: [
+       {name: 'playlistName1', 
+      artist: 'playlistArtist1', 
+      album: 'playlistAlbum1', 
+      id: 4}, 
+
+      {name: 'playlistName2', 
+      artist: 'playlistArtist2', 
+      album: 'playlistAlbum2', 
+      id: 5}, 
+
+      {name: 'playlistName3', 
+      artist: 'playlistArtist3', 
+      album: 'playlistAlbum3', 
+      id: 6}]
+
     }
   }
 
@@ -41,7 +58,8 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} 
+                      playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
