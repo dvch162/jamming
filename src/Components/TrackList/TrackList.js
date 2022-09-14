d5f42ff45a054fff830e218174dbc8b099903ac0
Dmytro Che <deefordis8@gmail.com>
2022-09-14 15:23:47 +0300

    Section 3

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,10 +2,18 @@ import React from "react";
 
 import './TrackList.css';
 
+import Track from "../Track/Track";
+
 class TrackList extends React.Component {
     render() {
         return (
-            <div class="TrackList">
+            <div className="TrackList">
+                {
+                    this.props.tracks&& this.props.tracks.map(track => {
+                      return  <Track track={track}
+                            key={track.id} />
+                    })
+                }
             </div>
         )
     }
